c80442d5c70f3e6f7ff5a827f648ac8f012c3d69
CrovaxVI <scarpante.niccolo@iisgalvanimi.edu.it>
2021-01-29 12:52:28 +0000

    routes1

--- EdCivica/routes/users.js
+++ EdCivica/routes/users.js
@@ -1,18 +1,48 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function (req, res, next) {
+
+router.get('/1', function (req, res, next) {
     const uri = TUA_STRINGA_DI_CONNESSIONE
     const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
     client.connect(err => {
-        const collection = client.db("sample_mflix").collection("movies"); //Mi connetto alla collection movies
-        // perform actions on the collection object
+        const collection = client.db("dati").collection("tubercolosi"); 
+     
         collection.find().limit(10).toArray((err, result) => {
-            if (err) console.log(err.message); //Se c'è qualche errore lo stampo
+            if (err) console.log(err.message); 
             else console.log(result);
-            client.close(); //Quando ho terminato la find chiudo la sessione con il db
-        }); //Eseguo la query e passo una funzione di callback
+            client.close(); 
+        }); 
+
+    });
+    res.send('respond with a resource');
+});
+router.get('/2', function (req, res, next) {
+    const uri = TUA_STRINGA_DI_CONNESSIONE
+    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+    client.connect(err => {
+        const collection = client.db("dati").collection("uhc"); 
+   
+        collection.find().limit(10).toArray((err, result) => {
+            if (err) console.log(err.message); 
+            else console.log(result);
+            client.close(); 
+        }); 
+
+    });
+    res.send('respond with a resource');
+});
+router.get('/3', function (req, res, next) {
+    const uri = TUA_STRINGA_DI_CONNESSIONE
+    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+    client.connect(err => {
+        const collection = client.db("dati").collection("vaccini");
+     
+        collection.find().limit(10).toArray((err, result) => {
+            if (err) console.log(err.message); 
+            else console.log(result);
+            client.close(); 
+        }); 
 
     });
     res.send('respond with a resource');
